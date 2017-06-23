a9df7835f6a418c3d2f550e673c5aaca11de67ff
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-06-23 13:33:18 +0200

    include feedback form

--- src/main/java/Tipmailer.java
+++ src/main/java/Tipmailer.java
@@ -9,6 +9,10 @@ import java.util.stream.Collectors;
 public class Tipmailer {
 
   private static final Random RANDOM = new Random();
+  private static final String[] FEEDBACK_MESSAGES = {
+    "It would be great to get feedback about yesterday",
+    "What do you think about yesterday's tip",
+  };
 
   public static void main(String[] args) throws IOException {
     MailSender mailSender = new MailSender();
@@ -19,7 +23,12 @@ public class Tipmailer {
 
   private static void send(MailSender mailSender, String subscriber, String randomTip) {
     System.out.println("sending to " + subscriber + ": " + randomTip);
-    mailSender.sendMessage(MailSender.createEmail(subscriber, "Collective Intelligence - Tip of the day", randomTip));
+    mailSender.sendMessage(MailSender.createEmail(subscriber, "Collective Intelligence - Tip of the day", mailBody(randomTip)));
+  }
+
+  private static String mailBody(String randomTip) {
+    String feedbackMessage = FEEDBACK_MESSAGES[RANDOM.nextInt(FEEDBACK_MESSAGES.length)];
+    return randomTip + "\n\n" + feedbackMessage + ": https://goo.gl/forms/z4VpbWEwJW3POOUN2";
   }
 
   private static String getRandomTip(List<Path> tips) {
